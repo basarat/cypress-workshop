4b6a8bd63a33cf7e5e61a5a26f8ed8679719b152
Basarat <basaratali@gmail.com>
2020-06-03 15:06:18 +1000

    update to use react-scripts

--- src/cypress/plugins/index.js
+++ src/cypress/plugins/index.js
@@ -1,9 +1,5 @@
 // cypress/plugins/index.js
 module.exports = (on, config) => {
-  // from the root of the project (folder with cypress.json file)
-  config.env.webpackFilename = 'webpack.config.js'
-  require('cypress-react-unit-test/plugins/load-webpack')(on, config)
-  // IMPORTANT to return the config object
-  // with the any changed environment variables
-  return config
-}
+  require('cypress-react-unit-test/plugins/react-scripts')(on, config);
+  return config;
+};
