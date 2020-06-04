a45adf42bc333ef67e083743384a30c1612e578b
Basarat <basaratali@gmail.com>
2020-06-04 11:04:26 +1000

    fix

--- src/cypress/integration/example.ts
+++ src/cypress/integration/example.ts
@@ -2,6 +2,6 @@ import * as assert from 'assert';
 
 describe('Example', () => {
   it('Should work', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('http://localhost:3000');
   });
 });
